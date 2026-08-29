a4d82285f58ee559ac81cb824ddbb841a9fc1ec0
bhaveshramanuj28 <bhavesh.ramanuj@jbullsinfotech.com>
2026-08-29 19:20:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,18 +6,16 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 let rooms = {};
-let users = {};
 
-function genRoom() {
+function makeRoom() {
   return Math.random().toString(36).substring(2, 8);
 }
 
 io.on("connection", (socket) => {
 
-  // create room
+  // CREATE ROOM
   socket.on("createRoom", (password, cb) => {
-
-    let roomId = genRoom();
+    const roomId = makeRoom();
 
     rooms[roomId] = {
       password,
@@ -27,7 +25,7 @@ io.on("connection", (socket) => {
     cb({ roomId });
   });
 
-  // join room
+  // JOIN ROOM
   socket.on("join", ({ room, password, user }) => {
 
     if (!rooms[room]) {
@@ -45,56 +43,27 @@ io.on("connection", (socket) => {
       return;
     }
 
-    socket.user = user;
     socket.room = room;
+    socket.user = user;
 
     rooms[room].users.push(socket.id);
     socket.join(room);
 
-    users[socket.id] = user;
+    // 👇 important: initiator decide
+    const initiator = rooms[room].users[0] === socket.id;
 
-    io.to(room).emit("online", rooms[room].users.map(id => users[id]));
-
-    if (rooms[room].users.length === 2) {
-      io.to(room).emit("ready");
-    }
+    io.to(room).emit("start", { initiator });
   });
 
-  // CHAT
-  socket.on("msg", ({ room, data }) => {
-    socket.to(room).emit("msg", {
-      user: socket.user,
-      data
-    });
-  });
-
-  // FILE
-  socket.on("file", ({ room, file }) => {
-    socket.to(room).emit("file", file);
-  });
-
-  // TYPING
-  socket.on("typing", (room) => {
-    socket.to(room).emit("typing", socket.user);
-  });
-
-  // WEBRTC SIGNALING
+  // SIGNALING
   socket.on("offer", d => socket.to(d.room).emit("offer", d.offer));
   socket.on("answer", d => socket.to(d.room).emit("answer", d.answer));
   socket.on("ice", d => socket.to(d.room).emit("ice", d.candidate));
 
-  // DISCONNECT
+  // DISCONNECT CLEANUP
   socket.on("disconnect", () => {
-
-    delete users[socket.id];
-
     for (let r in rooms) {
       rooms[r].users = rooms[r].users.filter(id => id !== socket.id);
-
-      io.to(r).emit(
-        "online",
-        rooms[r].users.map(id => users[id])
-      );
     }
   });
 
