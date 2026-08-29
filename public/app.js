82969faf17881760b60b5c947de0260b085903df
bhaveshramanuj28 <bhavesh.ramanuj@jbullsinfotech.com>
2026-08-29 19:50:30 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -10,14 +10,18 @@ function join() {
   room = document.getElementById("room").value;
 
   socket.emit("join", { name, room }, (res) => {
-    if (!res.ok) return alert(res.error);
+
+    if (!res.ok) {
+      alert(res.error);
+      return;
+    }
 
     document.getElementById("login").style.display = "none";
     document.getElementById("chat").style.display = "block";
   });
 }
 
-// SEND TEXT
+// SEND MSG
 function send() {
 
   let msg = document.getElementById("msg").value;
@@ -27,12 +31,19 @@ function send() {
   document.getElementById("msg").value = "";
 }
 
-// RECEIVE TEXT
+// RECEIVE MSG
 socket.on("msg", d => {
-  add(d.name + ": " + d.msg, "text");
+
+  let div = document.createElement("div");
+
+  div.className = "msg " + (d.name === name ? "me" : "other");
+
+  div.innerHTML = `<b>${d.name}</b><br>${d.msg}`;
+
+  document.getElementById("messages").appendChild(div);
 });
 
-// FILE SEND (IMAGE + FILE + PREVIEW)
+// FILE + IMAGE PREVIEW
 function sendFile() {
 
   let file = document.getElementById("file").files[0];
@@ -44,11 +55,10 @@ function sendFile() {
     socket.emit("file", {
       room,
       file: reader.result,
-      fileType: file.type
+      type: file.type
     });
 
-    // local preview
-    showPreview(reader.result, file.type);
+    showFile(reader.result, file.type);
   };
 
   reader.readAsDataURL(file);
@@ -56,46 +66,86 @@ function sendFile() {
 
 // RECEIVE FILE
 socket.on("file", d => {
-
-  if (d.fileType.startsWith("image")) {
-    showPreview(d.file, d.fileType);
-  } else if (d.fileType.startsWith("audio")) {
-    let audio = new Audio(d.file);
-    audio.controls = true;
-    document.getElementById("messages").appendChild(audio);
-  } else {
-    let a = document.createElement("a");
-    a.href = d.file;
-    a.download = "file";
-    a.innerText = "Download File";
-    document.getElementById("messages").appendChild(a);
-  }
+  showFile(d.file, d.type);
 });
 
-// PREVIEW FUNCTION
-function showPreview(file, type) {
+// FILE PREVIEW SYSTEM
+function showFile(file, type) {
 
   let div = document.createElement("div");
 
   if (type.startsWith("image")) {
     div.innerHTML = `<img src="${file}">`;
-  } else {
-    div.innerText = "File received";
+  }
+  else if (type.startsWith("audio")) {
+    div.innerHTML = `<audio controls src="${file}"></audio>`;
+  }
+  else {
+    div.innerHTML = `<a href="${file}" download>Download File</a>`;
   }
 
-  document.getElementById("preview").appendChild(div);
+  document.getElementById("messages").appendChild(div);
 }
 
-// ONLINE
+// ONLINE USERS
 socket.on("online", users => {
   document.getElementById("online").innerText =
     "Online: " + users.join(", ");
 });
 
-// UI
-function add(text) {
-  let div = document.createElement("div");
-  div.className = "msg";
-  div.innerText = text;
-  document.getElementById("messages").appendChild(div);
+---
+
+# 📞 CALL (STABLE WEBRTC)
+
+async function call() {
+
+  pc = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+  });
+
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  });
+
+  document.getElementById("local").srcObject = stream;
+
+  stream.getTracks().forEach(t => pc.addTrack(t, stream));
+
+  pc.ontrack = e => {
+    document.getElementById("remote").srcObject = e.streams[0];
+  };
+
+  pc.onicecandidate = e => {
+    if (e.candidate) {
+      socket.emit("ice", { room, candidate: e.candidate });
+    }
+  };
+
+  let offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+
+  socket.emit("offer", { room, offer });
 }
+
+// RECEIVE CALL
+socket.on("offer", async offer => {
+
+  pc = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+  });
+
+  pc.ontrack = e => {
+    document.getElementById("remote").srcObject = e.streams[0];
+  };
+
+  await pc.setRemoteDescription(offer);
+
+  let ans = await pc.createAnswer();
+  await pc.setLocalDescription(ans);
+
+  socket.emit("answer", { room, answer: ans });
+});
+
+socket.on("answer", ans => pc.setRemoteDescription(ans));
+socket.on("ice", c => pc.addIceCandidate(c));
