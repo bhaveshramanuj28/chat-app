ef2d00f7db8d9caf3e38a6793b79a8506021a88a
bhaveshramanuj28 <bhavesh.ramanuj@jbullsinfotech.com>
2026-08-29 19:45:36 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,27 +1,23 @@
 const socket = io();
 
-let room;
-let name;
+let room, name;
+let pc;
 
-// JOIN (FIXED CALLBACK SAFE)
+// JOIN
 function join() {
 
   name = document.getElementById("name").value;
   room = document.getElementById("room").value;
 
   socket.emit("join", { name, room }, (res) => {
-
-    if (!res.ok) {
-      document.getElementById("error").innerText = res.error;
-      return;
-    }
+    if (!res.ok) return alert(res.error);
 
     document.getElementById("login").style.display = "none";
     document.getElementById("chat").style.display = "block";
   });
 }
 
-// SEND MESSAGE
+// SEND TEXT
 function send() {
 
   let msg = document.getElementById("msg").value;
@@ -31,14 +27,69 @@ function send() {
   document.getElementById("msg").value = "";
 }
 
-// RECEIVE MESSAGE
-socket.on("msg", (d) => {
-  add(d.name + ": " + d.msg);
+// RECEIVE TEXT
+socket.on("msg", d => {
+  add(d.name + ": " + d.msg, "text");
 });
 
-// SYSTEM
-socket.on("system", (m) => {
-  document.getElementById("system").innerText = m;
+// FILE SEND (IMAGE + FILE + PREVIEW)
+function sendFile() {
+
+  let file = document.getElementById("file").files[0];
+
+  let reader = new FileReader();
+
+  reader.onload = () => {
+
+    socket.emit("file", {
+      room,
+      file: reader.result,
+      fileType: file.type
+    });
+
+    // local preview
+    showPreview(reader.result, file.type);
+  };
+
+  reader.readAsDataURL(file);
+}
+
+// RECEIVE FILE
+socket.on("file", d => {
+
+  if (d.fileType.startsWith("image")) {
+    showPreview(d.file, d.fileType);
+  } else if (d.fileType.startsWith("audio")) {
+    let audio = new Audio(d.file);
+    audio.controls = true;
+    document.getElementById("messages").appendChild(audio);
+  } else {
+    let a = document.createElement("a");
+    a.href = d.file;
+    a.download = "file";
+    a.innerText = "Download File";
+    document.getElementById("messages").appendChild(a);
+  }
+});
+
+// PREVIEW FUNCTION
+function showPreview(file, type) {
+
+  let div = document.createElement("div");
+
+  if (type.startsWith("image")) {
+    div.innerHTML = `<img src="${file}">`;
+  } else {
+    div.innerText = "File received";
+  }
+
+  document.getElementById("preview").appendChild(div);
+}
+
+// ONLINE
+socket.on("online", users => {
+  document.getElementById("online").innerText =
+    "Online: " + users.join(", ");
 });
 
 // UI
