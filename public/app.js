98c84ece89502d718e20eb49dc77e046c1913c12
bhaveshramanuj28 <bhavesh.ramanuj@jbullsinfotech.com>
2026-08-29 19:10:18 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,90 +1,113 @@
 const socket = io();
 
+let pc;
+let channel;
 let room;
-let user;
+let channelReady = false;
 
-// AUTO JOIN FROM LINK (🔥 IMPORTANT FIX)
-window.onload = () => {
-  const params = new URLSearchParams(window.location.search);
-  const roomFromUrl = params.get("room");
-
-  if (roomFromUrl) {
-    document.getElementById("room").value = roomFromUrl;
-  }
-};
-
-// CREATE ROOM + INVITE LINK
-function createRoom() {
-  let pass = document.getElementById("password").value;
-
-  socket.emit("createRoom", pass, ({ roomId }) => {
-
-    const link = `${window.location.origin}?room=${roomId}`;
-
-    prompt("Share this invite link:", link);
-  });
-}
-
-// JOIN ROOM
+// JOIN
 function join() {
+
   room = document.getElementById("room").value;
   let password = document.getElementById("password").value;
-  user = document.getElementById("user").value;
 
-  socket.emit("join", { room, password, user });
+  socket.emit("join", { room, password });
 
   document.getElementById("login").style.display = "none";
   document.getElementById("chat").style.display = "block";
 }
 
-// ERRORS
-socket.on("wrongPassword", () => alert("Wrong password"));
-socket.on("invalidRoom", () => alert("Room not found"));
-socket.on("full", () => alert("Room full"));
+// CREATE PEER
+function createPeer() {
 
-// SEND MESSAGE
-function send() {
-  let msg = document.getElementById("msg").value;
-
-  socket.emit("msg", {
-    room,
-    data: msg
+  pc = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
   });
 
-  add("You: " + msg);
+  pc.onicecandidate = e => {
+    if (e.candidate) {
+      socket.emit("ice", { room, candidate: e.candidate });
+    }
+  };
+
+  pc.ondatachannel = (e) => {
+    channel = e.channel;
+
+    channel.onopen = () => {
+      channelReady = true;
+    };
+
+    channel.onmessage = (e) => {
+      add("Friend: " + e.data);
+    };
+  };
+}
+
+// START (USER A)
+socket.on("start", async () => {
+
+  createPeer();
+
+  channel = pc.createDataChannel("chat");
+
+  channel.onopen = () => {
+    channelReady = true;
+  };
+
+  channel.onmessage = (e) => {
+    add("Friend: " + e.data);
+  };
+
+  let offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+
+  socket.emit("offer", { room, offer });
+});
+
+// OFFER (USER B)
+socket.on("offer", async (offer) => {
+
+  createPeer();
+
+  await pc.setRemoteDescription(offer);
+
+  let answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+
+  socket.emit("answer", { room, answer });
+});
+
+// ANSWER
+socket.on("answer", async (answer) => {
+  await pc.setRemoteDescription(answer);
+});
+
+// ICE
+socket.on("ice", async (c) => {
+  try {
+    await pc.addIceCandidate(c);
+  } catch {}
+});
+
+// SEND MESSAGE (🔥 MAIN FIX)
+function send() {
+
+  let msg = document.getElementById("msg").value;
+
+  if (!channel || channel.readyState !== "open") {
+    alert("Connection not ready");
+    return;
+  }
+
+  channel.send(msg);
+  add("Me: " + msg);
 
   document.getElementById("msg").value = "";
 }
 
-// RECEIVE MESSAGE
-socket.on("msg", (data) => {
-  add(data.user + ": " + data.data);
-});
-
-// ONLINE USERS
-socket.on("onlineUsers", (users) => {
-  document.getElementById("users").innerHTML =
-    "Online: " + users.join(", ");
-});
-
-// TYPING
-function typing() {
-  socket.emit("typing", room);
-}
-
-socket.on("typing", (user) => {
-  document.getElementById("typing").innerText =
-    user + " is typing...";
-
-  setTimeout(() => {
-    document.getElementById("typing").innerText = "";
-  }, 1000);
-});
-
 // UI
 function add(msg) {
   let div = document.createElement("div");
-  div.className = "msg";
   div.innerText = msg;
   document.getElementById("messages").appendChild(div);
 }
