155d07d64fb41f2b84cb4b95efb153c2f40fd905
bhaveshramanuj28 <bhavesh.ramanuj@jbullsinfotech.com>
2026-08-29 19:57:00 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,147 +1,51 @@
 const socket = io();
 
 let room, name;
-let pc;
 
-// JOIN SAFE
+// CONNECT DEBUG
+socket.on("connect", () => {
+  console.log("🟢 SOCKET CONNECTED");
+});
+
+// JOIN
 function join() {
 
   name = document.getElementById("name").value;
   room = document.getElementById("room").value;
 
+  console.log("➡️ SENDING JOIN:", { name, room });
+
   socket.emit("join", { name, room }, (res) => {
-    if (!res.ok) return alert(res.error);
+
+    console.log("⬅️ JOIN RESPONSE:", res);
+
+    if (!res || !res.ok) {
+      document.getElementById("error").innerText = res?.error || "Unknown error";
+      return;
+    }
 
     document.getElementById("login").style.display = "none";
     document.getElementById("chat").style.display = "block";
   });
 }
 
-// TEXT
+// SEND
 function send() {
 
   let msg = document.getElementById("msg").value;
 
-  socket.emit("msg", { room, msg });
+  socket.emit("msg", { msg });
 
   document.getElementById("msg").value = "";
 }
 
-// RECEIVE TEXT
-socket.on("msg", d => add(d.name + ": " + d.msg));
-
-// SYSTEM
-socket.on("system", m => {
-  document.getElementById("system").innerText = m;
-});
-
-// FILE + IMAGE + AUDIO
-function sendFile() {
-
-  let file = document.getElementById("file").files[0];
-
-  let reader = new FileReader();
-
-  reader.onload = () => {
-
-    socket.emit("file", {
-      room,
-      file: reader.result,
-      type: file.type
-    });
-
-    showFile(reader.result, file.type);
-  };
-
-  reader.readAsDataURL(file);
-}
-
-// RECEIVE FILE
-socket.on("file", d => {
-  showFile(d.file, d.type);
-});
-
-// FILE PREVIEW
-function showFile(file, type) {
+// RECEIVE
+socket.on("msg", d => {
 
   let div = document.createElement("div");
 
-  if (type.startsWith("image")) {
-    div.innerHTML = `<img src="${file}">`;
-  }
-
-  else if (type.startsWith("audio")) {
-    div.innerHTML = `<audio controls src="${file}"></audio>`;
-  }
-
-  else {
-    div.innerHTML = `<a href="${file}" download>Download File</a>`;
-  }
-
-  document.getElementById("messages").appendChild(div);
-}
-
-// UI
-function add(text) {
-  let div = document.createElement("div");
   div.className = "msg";
-  div.innerText = text;
+  div.innerText = `${d.name}: ${d.msg}`;
+
   document.getElementById("messages").appendChild(div);
-}
-
----
-
-# 📞 CALL (STABLE)
-
-async function call() {
-
-  pc = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-  });
-
-  let stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-  });
-
-  document.getElementById("local").srcObject = stream;
-
-  stream.getTracks().forEach(t => pc.addTrack(t, stream));
-
-  pc.ontrack = e => {
-    document.getElementById("remote").srcObject = e.streams[0];
-  };
-
-  pc.onicecandidate = e => {
-    if (e.candidate) {
-      socket.emit("ice", { room, candidate: e.candidate });
-    }
-  };
-
-  let offer = await pc.createOffer();
-  await pc.setLocalDescription(offer);
-
-  socket.emit("offer", { room, offer });
-}
-
-// RECEIVE CALL
-socket.on("offer", async offer => {
-
-  pc = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-  });
-
-  pc.ontrack = e => {
-    document.getElementById("remote").srcObject = e.streams[0];
-  };
-
-  await pc.setRemoteDescription(offer);
-
-  let ans = await pc.createAnswer();
-  await pc.setLocalDescription(ans);
-
-  socket.emit("answer", { room, answer: ans });
 });
-
-socket.on("answer", ans => pc.setRemoteDescription(ans));
-socket.on("ice", c => pc.addIceCandidate(c));
