22c767e98302754e137ac9252a563b0529a32f0f
bhaveshramanuj28 <bhavesh.ramanuj@jbullsinfotech.com>
2026-08-29 19:14:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,33 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 let rooms = {};
+let users = {};
+
+function genRoom() {
+  return Math.random().toString(36).substring(2, 8);
+}
 
 io.on("connection", (socket) => {
 
-  socket.on("join", ({ room, password }) => {
+  // create room
+  socket.on("createRoom", (password, cb) => {
+
+    let roomId = genRoom();
+
+    rooms[roomId] = {
+      password,
+      users: []
+    };
+
+    cb({ roomId });
+  });
+
+  // join room
+  socket.on("join", ({ room, password, user }) => {
 
     if (!rooms[room]) {
-      rooms[room] = { password, users: [] };
+      socket.emit("invalidRoom");
+      return;
     }
 
     if (rooms[room].password !== password) {
@@ -25,26 +45,59 @@ io.on("connection", (socket) => {
       return;
     }
 
+    socket.user = user;
+    socket.room = room;
+
     rooms[room].users.push(socket.id);
     socket.join(room);
 
-    socket.room = room;
+    users[socket.id] = user;
+
+    io.to(room).emit("online", rooms[room].users.map(id => users[id]));
 
     if (rooms[room].users.length === 2) {
-      io.to(room).emit("start");
+      io.to(room).emit("ready");
     }
   });
 
+  // CHAT
+  socket.on("msg", ({ room, data }) => {
+    socket.to(room).emit("msg", {
+      user: socket.user,
+      data
+    });
+  });
+
+  // FILE
+  socket.on("file", ({ room, file }) => {
+    socket.to(room).emit("file", file);
+  });
+
+  // TYPING
+  socket.on("typing", (room) => {
+    socket.to(room).emit("typing", socket.user);
+  });
+
+  // WEBRTC SIGNALING
   socket.on("offer", d => socket.to(d.room).emit("offer", d.offer));
   socket.on("answer", d => socket.to(d.room).emit("answer", d.answer));
   socket.on("ice", d => socket.to(d.room).emit("ice", d.candidate));
 
+  // DISCONNECT
   socket.on("disconnect", () => {
+
+    delete users[socket.id];
+
     for (let r in rooms) {
       rooms[r].users = rooms[r].users.filter(id => id !== socket.id);
+
+      io.to(r).emit(
+        "online",
+        rooms[r].users.map(id => users[id])
+      );
     }
   });
 
 });
 
-http.listen(10000, () => console.log("Running"));
+http.listen(10000, () => console.log("Server running"));
